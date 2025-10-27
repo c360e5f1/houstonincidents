8e8b921cc6840cf8624eb3861d1738d81049ad64
c360e5f1 <356292+c360e5f1@users.noreply.github.com>
2025-10-26 23:32:12 -0500

    Add timezone support for call time formatting

--- main.sh
+++ main.sh
@@ -3,6 +3,9 @@
 # Houston ArcGIS endpoint
 json_url="https://mycity2.houstontx.gov/pubgis01/rest/services/HEC/HEC_Active_Incidents/MapServer/0/query?f=json&cacheHint=true&resultOffset=0&resultRecordCount=500&where=1%3D1&orderByFields=CALL_TIME%20DESC&outFields=*&returnGeometry=false&spatialRel=esriSpatialRelIntersects"
 
+# Time Zone
+timezone="America/Chicago"
+
 # Perform some quick sanity checks to make sure the temp files that we need exist
 echo "Sanity checks and cleaning up..."
 echo "" > ./tmp/houstonincidentsdata.json.tmp
@@ -21,6 +24,7 @@ jq -c '.features[].attributes' ./tmp/houstonincidentsdata.json.tmp | while read 
   poi_address=$(echo $i | jq '.Address' | sed 's/\"//g')
   poi_crossstreet=$(echo $i | jq '.CrossStreet' | sed 's/\"//g')
   poi_calltime_unix=$(echo $i | jq '.CALL_TIME' | sed 's/\"//g')
+  poi_calltime=$(TZ=$timezone date "@$poi_calltime_unix")
   poi_incidenttype=$(echo $i | jq '.IncidentType' | sed 's/\"//g')
   poi_alarmlevel=$(echo $i | jq '.ALARM_LEVEL' | sed 's/\"//g')
   poi_numunits=$(echo $i | jq '.NO_UNITS' | sed 's/\"//g')
@@ -29,12 +33,12 @@ jq -c '.features[].attributes' ./tmp/houstonincidentsdata.json.tmp | while read 
   poi_lon=$(echo $i | jq '.LONGITUDE' | sed 's/\"//g')
 
   echo "var marker = L.marker([$poi_lat, $poi_lon]).addTo(map);" >> ./tmp/poi.dat
-  echo "marker.bindPopup("\""Request Type: $poi_agency<br>Address: $poi_address<br>Cross Street: $poi_crossstreet<br>Incident Type: $poi_incidenttype<br>Number of Units: $poi_numunits<br>Units: $poi_units"\"");" >> ./tmp/poi.dat
+  echo "marker.bindPopup("\""Request Type: $poi_agency<br>Address: $poi_address<br>Cross Street: $poi_crossstreet<br>Call Time: $poi_calltime<br>Incident Type: $poi_incidenttype<br>Number of Units: $poi_numunits<br>Units: $poi_units"\"");" >> ./tmp/poi.dat
 done
 
 # Concatonate our static header, POI index, and static footer to build the index.html document
 echo "Building index.html..."
-cat ./static/header.dat | sed "s/poots/$(TZ="America/Chicago" date -Iminutes)/" >> ./web/index.html
+cat ./static/header.dat | sed "s/poots/$(TZ=$timezone date -Iminutes)/" >> ./web/index.html
 cat ./tmp/poi.dat >> ./web/index.html
 cat ./static/footer.dat >> ./web/index.html
 
